ddb4729abcdffd2891d4317a100ff8b6835d3c58
Anirudh Sriram <hello@techwithanirudh.com>
2025-01-10 06:04:10 +0000

    fix: auth

--- site/src/lib/auth.ts
+++ site/src/lib/auth.ts
@@ -4,6 +4,16 @@ import { db } from "@/db";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "sqlite", 
-    })
+        provider: "sqlite",
+    }),
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        }
+    },
 });
